8aec4c9c176a6ea225ae3191490a13d91b8c02a7
Andreas Gohr <andi@splitbrain.org>
2009-02-27 14:40:38 +0100

    Added raw structure for SQLite helper

---
+++ helper/sqlite.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
+ */
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+
+class helper_plugin_blogtng_sqlite extends DokuWiki_Plugin {
+
+    var $db = null;
+
+    /**
+     * constructor
+     */
+    function helper_plugin_blogtng_sqlite(){
+        if (!extension_loaded('sqlite')) {
+            $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+            @dl($prefix . 'sqlite.' . PHP_SHLIB_SUFFIX);
+        }
+
+        if(!function_exists('sqlite_open')){
+            msg('blogtng plugin: SQLite support missing in this PHP install - plugin will not work',-1);
+        }
+    }
+
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return array(
+            'author' => 'Andreas Gohr',
+            'email'  => 'andi@splitbrain.org',
+            'date'   => '2009-02-13',
+            'name'   => 'Structured Data Plugin',
+            'desc'   => 'Add and query structured data in your wiki',
+            'url'    => 'http://www.dokuwiki.org/plugin:data',
+        );
+        //FIXME
+    }
+
+    /**
+     * Makes sure the given data fits with the given type
+     */
+    function _cleanData($value, $type){
+        $value = trim($value);
+        if(!$value) return '';
+        switch($type){
+            case 'page':
+                return cleanID($value);
+            case 'nspage':
+                return cleanID($value);
+            case 'dt':
+                if(preg_match('/^(\d\d\d\d)-(\d\d?)-(\d\d?)$/',$value,$m)){
+                    return sprintf('%d-%02d-%02d',$m[1],$m[2],$m[3]);
+                }
+                return '';
+            case 'url':
+                if(!preg_match('!^[a-z]+://!i',$value)) $value='http://'.$value;
+                return $value;
+            case 'mail':
+                $mail = '';
+                $name = '';
+                $part = '';
+                $parts = preg_split('/\s+/',$value);
+                do{
+                    $part = array_shift($parts);
+                    if(!$email && mail_isvalid($part)){
+                        $email = strtolower($part);
+                        continue;
+                    }
+                    $name .= $part.' ';
+                }while($part);
+                return trim($email.' '.$name);
+            default:
+                return $value;
+        }
+    }
+
+
+    /**
+     * Open the database
+     */
+    function _dbconnect(){
+        global $conf;
+
+        $dbfile = $conf['metadir'].'/blogplugin.sqlite';
+        $init   = (!@file_exists($dbfile) || !@filesize($dbfile));
+
+        $error='';
+        $this->db = sqlite_open($dbfile, 0666, $error);
+        if(!$this->db){
+            msg("blogtng plugin: failed to open SQLite database ($error)",-1);
+            return false;
+        }
+
+        if($init) $this->_initdb();
+        return true;
+    }
+
+
+    /**
+     * create the needed tables
+     */
+    function _initdb(){
+/*
+        sqlite_query($this->db,'CREATE TABLE pages (pid INTEGER PRIMARY KEY, page, title);');
+        sqlite_query($this->db,'CREATE UNIQUE INDEX idx_page ON pages(page);');
+        sqlite_query($this->db,'CREATE TABLE data (eid INTEGER PRIMARY KEY, pid INTEGER, key, value);');
+        sqlite_query($this->db,'CREATE INDEX idx_key ON data(key);');
+*/
+    }
+}
