48b1131632728c776c0c4379f9d99f8309df448d
Gina Haeussge <gina@foosel.net>
2009-02-27 21:34:14 +0100

    reformatted save queries

--- helper/entry.php
+++ helper/entry.php
@@ -62,9 +62,29 @@ class helper_plugin_blogtng_entry extends DokuWiki_Plugin {
      */
     function save() {
         $query = 'INSERT OR IGNORE INTO articles (pid, page, title, image, created, lastmod, author, login, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $result = $this->sqlite->query($query, $this->entry['pid'], $this->entry['page'], $this->entry['title'], $this->entry['image'], $this->entry['created'], $this->entry['lastmod'], $this->entry['author'], $this->entry['login']);
+        $result = $this->sqlite->query(
+            $query,
+            $this->entry['pid'],
+            $this->entry['page'],
+            $this->entry['title'],
+            $this->entry['image'],
+            $this->entry['created'],
+            $this->entry['lastmod'],
+            $this->entry['author'],
+            $this->entry['login']
+        );
         $query = 'UPDATE articles SET page=?, title=?, image=?, lastmod=?, author=?, login=?, email=? WHERE pid=?';
-        $result = $this->sqlite->query($query, $this->entry['page'], $this->entry['title'], $this->entry['image'], $this->entry['lastmod'], $this->entry['author'], $this->entry['login'], $this->entry['email'], $this->entry['pid']);
+        $result = $this->sqlite->query(
+            $query,
+            $this->entry['page'],
+            $this->entry['title'],
+            $this->entry['image'],
+            $this->entry['lastmod'],
+            $this->entry['author'],
+            $this->entry['login'],
+            $this->entry['email'],
+            $this->entry['pid']
+        );
         if(!$result) {
             msg('blogtng plugin: failed to save new entry!', -1);
             return false;
