dd8c568c10fb780da94ed1de088c6ba8b51ccfd0
Andreas Gohr <andi@splitbrain.org>
2009-02-28 11:16:05 +0100

    first template examples created

---
+++ tpl/list_full.php
@@ -0,0 +1,10 @@
+<div class="blog_post">
+<?php $entry->tpl_article('read more...')?>
+
+<p class="blog_footer">
+    <?php $entry->tpl_commentcount('(%d Comment)','(%d Comments)')?>
+    &midot;
+    <?php $entry->tpl_created('%Y-%m-%d')?>
+</p>
+
+</div>
---
+++ tpl/list_small.php
@@ -0,0 +1,10 @@
+<h1>
+<?php $entry->tpl_headline()?>
+<?php $entry->tpl_commentcount('(%d Comment)','(%d Comments)',false)?>
+</h1>
+
+<p>
+<?php $entry->tpl_abstract(50)?><br />
+<?php $entry->tpl_permalink('read more...')?>
+</p>
+
