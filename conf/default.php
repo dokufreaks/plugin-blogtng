a06d0fbf55f0b3d4dd9aacafad00bb1820358db5
Michael Klier <chi@chimeric.de>
2009-07-28 12:48:07 +0200

    allow to set date via edit form

--- conf/default.php
+++ conf/default.php
@@ -4,3 +4,4 @@ $conf['comments_subscription']    = 0;
 $conf['comments_gravatar_rating'] = 'R';
 $conf['comments_forbid_syntax']   = '';
 $conf['comments_xhtml_renderer']  = '';
+$conf['editfrom_set_date']        = 0;
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,3 +4,4 @@ $lang['comments_subscription']    = 'Allow users to subscribe to new comments vi
 $lang['comments_gravatar_rating'] = 'Minimum rating for gravatars.';
 $lang['comments_xhtml_renderer']  = 'Syntax plugins to be renderer in comments via the default XHTML renderer instead of the special comment renderer. Use this to support syntax of plugins not natively supporting the comment renderer.';
 $lang['comments_forbid_syntax']   = 'Syntax plugins to be completely forbidden in comments (neither in XHTML nor in comment rendition).';
+$lang['editform_set_date']        = 'Allow to set the creation date of a blog entry via the edit form.';
