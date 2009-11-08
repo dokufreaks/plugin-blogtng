25828e9148094592c299b66e0764d3053e3c5e65
Andreas Gohr <andi@splitbrain.org>
2009-11-08 12:45:12 +0100

    minor layout tweak

--- tpl/default_entry.php
+++ tpl/default_entry.php
@@ -15,9 +15,9 @@
     <div class="blogtng_footer level1">
         This blog post was created <?php $entry->tpl_created('on %Y-%m-%d at %H:%M')?> by
         <?php $entry->tpl_author()?>.
-        <?php if ($entry->has_tags()) {?>
+        <?php if ($entry->has_tags()):?>
             It is tagged with <?php $entry->tpl_tags('')?>.
-        <?php } ?>
+        <?php endif ?>
     </div>
     <?php if ($entry->entry['commentstatus'] != 'disabled') {?>
         <h2 id="the__comments">Comments</h2>
