0566beecfe71b28e5431138be5a151a5f9b390bd
Michael Klier <chi@chimeric.de>
2009-11-11 10:10:53 +0100

    allow to feed multiple blogs at once

--- action/feed.php
+++ action/feed.php
@@ -42,7 +42,7 @@ class action_plugin_blogtng_feed extends DokuWiki_Action_Plugin{
         $opt =& $event->data['opt'];
         if ($opt['feed_mode'] != 'blogtng') return;
 
-        $opt['blog'] = $_REQUEST['blog'];
+        $opt['blog'] = implode(',', $_REQUEST['blog']);
         $opt['sortby'] = $_REQUEST['sortby'];
         $opt['sortorder'] = $_REQUEST['sortorder'];
     }
@@ -54,7 +54,7 @@ class action_plugin_blogtng_feed extends DokuWiki_Action_Plugin{
         $event->preventDefault();
         $event->data['data'] = array();
         $conf = array(
-            'blog' => array($opt['blog']),
+            'blog' => $opt['blog'],
             'sortby' => $opt['sortby'],
             'sortorder' => $opt['sortorder'],
             'limit' => $opt['items'],
