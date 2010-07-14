9ac615f3217f723cdab388f7961545159d9390a5
Adrian Lang <mail@adrianlang.de>
2010-07-14 09:40:56 +0200

    Use real ellipsis

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -20,7 +20,7 @@ $lang['comments']    = 'Comments';
 $lang['0comments']   = '%d Comments';
 $lang['1comments']   = '%d Comment';
 $lang['Xcomments']   = '%d Comments';
-$lang['readmore']    = 'Read more...';
+$lang['readmore']    = 'Read more…';
 
 $lang['prev'] = '&lt; Newer Posts';
 $lang['next'] = 'Older Posts &gt;';
