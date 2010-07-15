dokufreaks/plugin-blogtng

commit fa85e39d530eef658da31fd9f2fe822bd44b0351

Move templates to their own directories

helper/entry.php
@@ -370,14 +370,10 @@ class helper_plugin_blogtng_entry extends DokuWiki_Plugin {
     //~~ template methods
 
     function tpl_content($name, $type) {
-        $whitelist = array('list', 'entry', 'feed');
-        if(!in_array($type, $whitelist)) return;
-        $tpl = DOKU_PLUGIN . 'blogtng/tpl/' . $name . '_' . $type . '.php';
-        if(file_exists($tpl)) {
+        $tpl = helper_plugin_blogtng_tools::getTplFile($name, $type);
+        if($tpl !== false) {
             $entry = $this;
             include($tpl);
-        } else {
-            msg('blogtng plugin: template ' . $tpl . ' does not exist!', -1);
         }
     }
 
@@ -627,8 +623,8 @@ class helper_plugin_blogtng_entry extends DokuWiki_Plugin {
     //~~ utility methods
 
     function get_blogs() {
-        $pattern = DOKU_PLUGIN . 'blogtng/tpl/*_entry.php';
-        $files = glob($pattern);
+        $pattern = DOKU_PLUGIN . 'blogtng/tpl/*{_,/}entry.php';
+        $files = glob($pattern, GLOB_BRACE);
         $blogs = array('');
         foreach ($files as $file) {
             array_push($blogs, substr($file, strlen(DOKU_PLUGIN . 'blogtng/tpl/'), -10));

helper/tools.php
@@ -80,4 +80,24 @@ class helper_plugin_blogtng_tools extends DokuWiki_Plugin {
         }
     }
 
+    static public function getTplFile($tpl, $type) {
+        $whitelist = array('list', 'entry', 'feed');
+        if(!in_array($type, $whitelist)) return false;
+
+        $res = false;
+        foreach(array('/', '_') as $sep) {
+            $fname = DOKU_PLUGIN . "blogtng/tpl/$tpl$sep$type.php";
+            if (file_exists($fname)) {
+                $res = $fname;
+                break;
+            }
+        }
+
+        if($res === false){
+            msg("blogtng plugin: template file $type for template $tpl does not exist!", -1);
+            return false;
+        }
+
+        return $res;
+    }
 }

helper/comments.php
@@ -516,9 +516,8 @@ class helper_plugin_blogtng_comments extends DokuWiki_Plugin {
         global $INFO;
 
         // check template
-        $tpl = DOKU_PLUGIN.'blogtng/tpl/'.$tpl.'_recentcomments.php';
-        if(!file_exists($tpl)){
-            msg('blogtng plugin: template ' . $tpl . ' does not exist!', -1);
+        $tpl = helper_plugin_blogtng_tools::getTplFile($tpl, 'recentcomments');
+        if($tpl === false){
             return false;
         }
 
@@ -575,16 +574,15 @@ class blogtng_comment{
     function output($name){
         global $INFO;
         $name = preg_replace('/[^a-zA-Z_\-]+/','',$name);
-        $tpl = DOKU_PLUGIN . 'blogtng/tpl/' . $name . '_comments.php';
-        if(file_exists($tpl)) {
-            $comment = $this;
-            if($comment->data['status'] == 'visible' || ($comment->data['status'] == 'hidden' && $INFO['isadmin'])) {
-                include($tpl);
-        }
-        } else {
-            msg('blogtng plugin: template ' . $tpl . ' does not exist!', -1);
+        $tpl = helper_plugin_blogtng_tools::getTplFile($name, 'comments');
+        if($tpl === false){
+            return false;
         }
 
+        $comment = $this;
+        if($comment->data['status'] == 'visible' || ($comment->data['status'] == 'hidden' && $INFO['isadmin'])) {
+            include($tpl);
+        }
     }
 
     function tpl_comment(){

commit 8a60de5cac5cc068d847a6ac148a2d6576c785ee

Move tpl whitelisting back to the entry helper
Fixes a bug introduced in fa85e39 where the comments tpl is not accessible anymore.

helper/entry.php
@@ -370,6 +370,9 @@ class helper_plugin_blogtng_entry extends DokuWiki_Plugin {
     //~~ template methods
 
     function tpl_content($name, $type) {
+        $whitelist = array('list', 'entry', 'feed');
+        if(!in_array($type, $whitelist)) return false;
+
         $tpl = helper_plugin_blogtng_tools::getTplFile($name, $type);
         if($tpl !== false) {
             $entry = $this;

helper/tools.php
@@ -81,9 +81,6 @@ class helper_plugin_blogtng_tools extends DokuWiki_Plugin {
     }
 
     static public function getTplFile($tpl, $type) {
-        $whitelist = array('list', 'entry', 'feed');
-        if(!in_array($type, $whitelist)) return false;
-
         $res = false;
         foreach(array('/', '_') as $sep) {
             $fname = DOKU_PLUGIN . "blogtng/tpl/$tpl$sep$type.php";