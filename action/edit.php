53646c7d0dd2d0186b6ee1935b0c8b84ef94e38f
Dennis Ploeger <develop@dieploegers.de>
2010-12-07 11:46:16 +0100

    Set page in the entry helper. This probably fixes issue #43.

--- action/edit.php
+++ action/edit.php
@@ -149,6 +149,12 @@ class action_plugin_blogtng_edit extends DokuWiki_Action_Plugin{
                 $this->entryhelper->entry['blog'] = $blog;
                 $this->entryhelper->entry['commentstatus'] = $this->tools->getParam('post/commentstatus');
 
+                if (empty($this->entryhelper->entry['page'])) {
+
+                    $this->entryhelper->entry['page'] = $ID;
+
+                }
+
                 // allow to override created date
                 if($this->tools->getParam('post/date') && $this->getConf('editform_set_date')) {
                     foreach(array('hh', 'mm', 'MM', 'DD') as $key) {
