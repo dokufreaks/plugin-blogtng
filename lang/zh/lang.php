5bdbb35667f21ebf45401e803b79efe8f8f925ca
lainme <lainme993@gmail.com>
2011-02-08 19:26:39 +0100

    Added simplified Chinese translation

---
+++ lang/zh/lang.php
@@ -0,0 +1,64 @@
+<?php
+
+$lang['menu'] = '管理博客(BlogTNG)';
+
+$lang['entry_latest']   = '最近 %d 篇文章';
+$lang['comment_latest'] = '最近 %d 条评论';
+$lang['numhits'] = '数量';
+
+$lang['blog']    = '博客';
+$lang['author']  = '作者';
+$lang['dashboard'] = '控制板';
+$lang['entry']   = '文章';
+$lang['filter']  = '过滤器';
+$lang['tags']    = '标签';
+$lang['commentstatus'] = '评论状态';
+
+$lang['searchresults']  = '搜索结果';
+
+$lang['comments']    = '评论';
+$lang['0comments']   = '%d 条评论';
+$lang['1comments']   = '%d 条评论';
+$lang['Xcomments']   = '%d 条评论';
+$lang['readmore']    = '继续阅读…';
+
+$lang['prev'] = '&lt; 更新的文章';
+$lang['next'] = '更早的文章 &gt;';
+
+$lang['title']   = '标题：';
+$lang['create']  = '创建';
+$lang['created'] = '创建日期';
+
+$lang['js']['reply'] = '回复这条评论';
+
+$lang['comment_mail']      = '邮件';
+$lang['comment_avatar']    = '头像';
+$lang['comment_name']      = '昵称';
+$lang['comment_web']       = '网站';
+$lang['comment_preview']   = '预览';
+$lang['comment_submit']    = '评论';
+$lang['comment_source']    = '来源';
+$lang['comment_status']    = '状态';
+$lang['comment_text']      = '文本';
+$lang['comment_ip']        = 'IP地址';
+$lang['comment_subscribe'] = '订阅新评论';
+
+$lang['subscr_subject'] = '%s 上的新评论';
+$lang['optin_subject']  = '请确认您的订阅';
+
+$lang['optin_ok']  = '谢谢，您将通过邮件接收评论通知';
+$lang['optin_err'] = '无法找到这个 key，可能是复制/粘贴的错误？';
+
+$lang['unsubscribe_ok'] = '成功取消订阅。您将不会再收到这篇文章的新评论通知。';
+$lang['unsubscribe_err_key'] = '无法找到这个 key，可能是复制/粘贴的错误？';
+$lang['unsubscribe_err_other'] = '无法找到您的订阅，可能是复制/粘贴的错误？';
+
+$lang['btn_comment_edit']  = '编辑';
+$lang['act_comment_edit']  = '编辑评论';
+
+$lang['msg_comment_save']           = '评论的改动已经保存';
+$lang['msg_comment_status_change']  = '评论状态已改变';
+$lang['msg_comment_delete']         = '评论已被删除';
+$lang['msg_entry_blog_change']      = '文章所属的博客已改变';
+
+$lang['err_notitle'] = '没有给出文章标题！';
---
+++ lang/zh/settings.php
@@ -0,0 +1,11 @@
+<?php
+$lang['comments_allow_web']       = '允许用户将网站链接到评论上？';
+$lang['comments_subscription']    = '允许用户通过邮件订阅新评论？';
+$lang['comments_gravatar_rating'] = 'gravatar 的最低评级。';
+$lang['comments_gravatar_default'] = '对没有 gravatar 图像的用户所用的头像。';
+$lang['comments_xhtml_renderer']  = '评论中，用默认的 XHTML 渲染器而非特殊的评论渲染器来进行渲染的语法插件。以此支持那些并不原生支持评论渲染器的语法插件。';
+$lang['comments_forbid_syntax']   = '在评论中完全禁止的语法插件(无论是通过 XHTML 还是评论渲染器)。';
+$lang['editform_set_date']        = '允许通过编辑框设置博客文章的创建日期。';
+$lang['tags']                     = '允许的标签，以逗号分隔。如果留空，将允许所有标签。';
+$lang['receive_linkbacks']         = '接收发送给您博客文章的链接通告？';
+$lang['send_linkbacks']          = '向您博客文章中链接的页面发送通告？';
