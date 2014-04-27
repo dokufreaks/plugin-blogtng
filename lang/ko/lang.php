ddd25ac828d1b0012f4ba07ea0f971a8a4bd1aff
Myeongjin <aranet100@gmail.com>
2014-04-27 15:36:29 +0900

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Korean language file
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the files belows "/plugin-blogtng/lang/<your lang>/". If they
+ * are not existing, copy and translate the English ones. And don't forget to
+ * send any new translation or fix to the BlogTNG authors (contact information
+ * can be found at <http://www.dokuwiki.org/plugin:blogtng>). Thanks :-).
+ *
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['menu']           = '블로그 관리 (BlogTNG)';
+$lang['entry_latest']   = '마지막 항목 %d개';
+$lang['comment_latest'] = '마지막 덧글 %d개';
+$lang['numhits']        = '조회 수';
+$lang['blog']           = '블로그';
+$lang['author']         = '저자';
+$lang['dashboard']      = '대시보드';
+$lang['entry']          = '항목';
+$lang['filter']         = '필터';
+$lang['tags']           = '태그';
+$lang['commentstatus']  = '덧글은';
+$lang['searchresults']  = '검색 결과';
+$lang['permalink']      = '고유링크';
+
+$lang['comments']    = '덧글';
+$lang['0comments']   = '덧글 %d개';
+$lang['1comments']   = '덧글 %d개';
+$lang['Xcomments']   = '덧글 %d개';
+$lang['readmore']    = '더 읽기…';
+
+$lang['prev'] = '&lt; 새 게시물';
+$lang['next'] = '오래된 게시물 &gt;';
+
+$lang['title']   = '제목:';
+$lang['create']  = '만들기';
+$lang['created'] = '만든 날짜';
+
+$lang['js']['reply'] = '이 덧글에 답글';
+
+$lang['comment_mail']      = '이메일';
+$lang['comment_avatar']    = '아바타';
+$lang['comment_name']      = '이름';
+$lang['comment_web']       = '웹사이트';
+$lang['comment_preview']   = '미리 보기';
+$lang['comment_submit']    = '덧글 남기기';
+$lang['comment_source']    = '출처';
+$lang['comment_status']    = '상태';
+$lang['comment_text']      = '텍스트';
+$lang['comment_ip']        = 'IP 주소';
+$lang['comment_subscribe'] = '새 덧글 구독';
+$lang['comment_reply']     = '답글';
+
+$lang['subscr_subject'] = '%s에 대한 새 덧글';
+$lang['optin_subject']  = '구독을 확인하세요';
+
+$lang['optin_ok']  = '감사합니다. 이제 메일을 통해 덧글 업데이트를 받을 것입니다';
+$lang['optin_err'] = '이런. 해당 키를 찾을 수 없습니다. 아마도 복사해서 붙여넣기 오류인가요?';
+
+$lang['unsubscribe_ok']        = '구독 취소를 성공했습니다. 더 이상 이 게시물로의 새 덧글에 대해 알리지 않을 것입니다.';
+$lang['unsubscribe_err_key']   = '이런. 해당 키를 찾을 수 없습니다. 아마도 복사해서 붙여넣기 오류인가요?';
+$lang['unsubscribe_err_other'] = '아이구. 구독을 찾을 수 없습니다. 아마도 복사해서 붙여넣기 오류인가요?';
+
+$lang['btn_comment_edit'] = '편집';
+$lang['act_comment_edit'] = '덧글 편집';
+
+$lang['msg_comment_save']           = '덧글 바뀜이 저장되었습니다';
+$lang['msg_comment_status_change']  = '덧글 상태가 바뀌었습니다';
+$lang['msg_comment_delete']         = '덧글이 삭제되었습니다';
+$lang['msg_entry_blog_change']      = '항목의 블로그가 바뀌었습니다';
+
+$lang['err_notitle'] = '주어진 게시물 제목이 없습니다!';
